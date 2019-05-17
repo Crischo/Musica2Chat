f1a14c4bd6154fd71b4fbf07a943e963d48414c7
Crischo <crismasteroz777@gmail.com>
2019-05-17 18:34:51 -0500

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+
+//  api: 'http://192.168.0.107:80/server/'
+
+api: 'http://192.168.1.4:80/server/'
+//api: 'http://192.168.43.51:80/server/'
+
+};
